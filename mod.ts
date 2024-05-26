d09a4b62c95bd23ffdd3661d421c377391638ea6
Daiki48 <daiki48.engineer@gmail.com>
2024-05-26 22:34:29 +0900

    :label: Added function type

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@
 	* @returns random integer with the specified
 	*
 	*/
-export const generateRanInt = (digits: number) => {
+export const generateRanInt = (digits: number): number => {
 	const min = Math.pow(10, digits - 1);
 	const max = Math.pow(10, digits) - 1;
 	return Math.floor(Math.random() * (max - min + 1)) + min;
